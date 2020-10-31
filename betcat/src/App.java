c3d6454c5237b4e3625728b011682e474411a0d0
Mário Furtado <mariodacostafurtado@gmail.com>
2020-10-31 12:21:40 -0100

    Added the lib, and initial menu on intellij

---
+++ betcat/src/App.java
@@ -0,0 +1,9 @@
+import org.academiadecodigo.bootcamp.Prompt;
+
+public class App {
+
+    public static void main(String[] args) {
+        Prompt prompt = new Prompt(System.in, System.out);
+        Menu menu = new Menu(prompt);
+    }
+}
